458a0935b43bbc4fd91a7dcb4694c8cafbbf7aaf
clonerops <114849678+clonerops@users.noreply.github.com>
2023-06-19 10:41:26 -0700

    complete tables

--- product/models.py
+++ product/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 from account.models import User
 # Create your models here.
+class ProductGroup(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True, verbose_name="عنوان")
+    description = models.TextField(blank=True, null=True,verbose_name="توضیحات")
+    isDeleted = models.BooleanField(default=False, verbose_name="آیا محصول در لیست حذف شده ها باشد؟")
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateField(auto_now=True)
+    
+    class Meta:
+        db_table = "productgroups"
+        verbose_name="گروه محصول"
+        verbose_name_plural="گروه محصولات"
+        
+    def __str__(self) -> str:
+        return self.title
+    
 class Product(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True, verbose_name="عنوان")
-    description = models.TextField(verbose_name="توضیحات")
+    description = models.TextField(blank=True, null=True, verbose_name="توضیحات")
+    productgroupId = models.ForeignKey(ProductGroup, default=1, on_delete=models.CASCADE, related_name="product_group")
+    productSize = models.CharField(max_length=255, blank=True, null=True, verbose_name="سایز کالا")
     createdBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name="product_createdby", verbose_name="ایجاد شده توسط")
     isActive = models.BooleanField(default=True, verbose_name="آیا محصول فعال باشد؟")
     isStock = models.BooleanField(default=True, verbose_name="آیا محصول موجود می باشد؟")
@@ -19,3 +36,9 @@ class Product(models.Model):
     def __str__(self) -> str:
         return self.title
 
+class ProductPrice(models.Model):
+    productId = models.OneToOneField(Product, on_delete=models.CASCADE, verbose_name="شناسه کالا")
+    price = models.CharField(max_length=255, blank=True, null=True, verbose_name="قیمت کالا")
+    isDeleted = models.BooleanField(default=False, verbose_name="آیا محصول در لیست حذف شده ها باشد؟")
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateField(auto_now=True)
--- store/models.py
+++ store/models.py
@@ -2,9 +2,19 @@ from django.db import models
 from account.models import User
 from product.models import Product
 # Create your models here.
+
+TypeChoise = (
+    ("1", "عمومی"),
+    ("2", "واسط"),
+)
 class Store(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True, verbose_name="عنوان")
     description = models.TextField(verbose_name="توضیحات")
+    type = models.CharField(
+        max_length=255,
+        choices= TypeChoise,
+        default="1"
+    )
     createdBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name="store_createdby", verbose_name="ایجاد شده توسط")
     isActive = models.BooleanField(default=True, verbose_name="آیا انبار فعال باشد؟")
     isDeleted = models.BooleanField(default=False, verbose_name="آیا انبار در لیست حذف شده ها باشد؟")
@@ -25,7 +35,7 @@ class ProductStore(models.Model):
     productId = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="productId", verbose_name="شناسه محصول")
     storeId = models.ForeignKey(Store, on_delete=models.CASCADE, related_name="storeId", verbose_name="شناسه انبار")
     floorInventory = models.IntegerField(verbose_name="موجودی کف")
-    actualInventory = models.IntegerField(verbose_name="موجودی واقعی")
+    actualInventory = models.IntegerField(verbose_name="موجودی قابل سفارش")
     isActive = models.BooleanField(default=True, verbose_name="آیا فعال باشد")
     isDeleted = models.BooleanField(default=False, verbose_name="آیا در لیست حذف شده ها باشد؟")
     createdAt = models.DateTimeField(auto_now_add=True)
@@ -36,6 +46,3 @@ class ProductStore(models.Model):
         verbose_name="مدیریت انبار-محصول"
         verbose_name_plural="مدیریت انبارها-محصول ها"
 
-
-    def __str__(self) -> str:
-        return self.productId + " " + self.storeId
