d45aacb52e152578660d62dc0e9d2146f078310d
clonerops <114849678+clonerops@users.noreply.github.com>
2023-06-20 11:14:45 -0700

    customer and order urls

--- accounting/urls.py
+++ accounting/urls.py
@@ -23,5 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('product.urls')),
-    path('api/', include('store.urls'))
+    path('api/', include('store.urls')),
+    path('api/', include('customer.urls')),
+    path('api/', include('order.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
